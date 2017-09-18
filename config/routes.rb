e11297c56060004a863f07bfa1de6ae3ce298cb5
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-18 14:26:07 +0900

    improve devise sign_up login views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :questions
+
   devise_for :users
-  root 'top#index'
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'questions#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'top#index', as: :unauthenticated_root
+    end
+  end
 
   resources :favorite_questions, only: [:create,:destroy]
 
