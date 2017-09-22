6803d366fd175b0de452618421feb31205584be5
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-22 13:06:19 +0900

    Add User show view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 
   resources :favorite_questions, only: [:create,:destroy]
 
